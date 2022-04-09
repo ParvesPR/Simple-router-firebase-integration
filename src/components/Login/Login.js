6575bc042492d4c4cd86d17702333ae223086fd9
ParvesPR <parves2585@gmail.com>
2022-04-09 12:07:12 +0800

    create custom hook

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -3,6 +3,9 @@ import React from 'react';
 const Login = () => {
     return (
         <div>
+            <div style={{margin:"20px"}}>
+                <button>Google Sign in</button>
+            </div>
             <h2>Login Here</h2>
             <form>
                 <input type="email" name="" id="" placeholder='Your email' /> <br />
---
+++ src/hooks/useFirebase.js
@@ -0,0 +1,15 @@
+import { useEffect, useState } from "react";
+
+const useFirebase = () => {
+    const [user, setUser] = useState({});
+
+    useEffect(() => {
+
+    }, []);
+
+    const signInWithGoogle = () => {
+
+    };
+    return [user, setUser]
+};
+export default useFirebase;
