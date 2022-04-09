cc29c08b1c1d696339c7a56a011c91f55d5706e5
ParvesPR <parves2585@gmail.com>
2022-04-09 13:00:33 +0800

    sign out function added

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,15 +4,16 @@ import { Link } from 'react-router-dom';
 import useFirebase from '../../hooks/useFirebase';
 
 const Header = () => {
-    const { user } = useFirebase()
+    const { users, handleSignOut } = useFirebase()
     return (
         <nav>
             <Link to="/">Home</Link>
             <Link to="/register">Register</Link>
             <Link to="/products">Products</Link>
             <Link to="/contact">Contact</Link>
-            {user.uid ?
-                <button>Sign Out</button> :
+            <span>{users?.displayName  && users.displayName}</span>
+            {users?.uid ?
+                <button onClick={handleSignOut}>Sign Out</button> :
                 <Link to="/login">Login</Link>
             }
         </nav>
--- src/hooks/useFirebase.js
+++ src/hooks/useFirebase.js
@@ -1,23 +1,33 @@
-import { useState } from "react";
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+import { useEffect, useState } from "react";
+import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signOut } from "firebase/auth";
 import app from '../firebase.init';
 
 const auth = getAuth(app);
 const googleProvider = new GoogleAuthProvider();
 
 const useFirebase = () => {
-    const [user, setUser] = useState({});
+    const [users, setUsers] = useState({});
 
     const signInWithGoogle = () => {
         signInWithPopup(auth, googleProvider)
             .then(result => {
                 const user = result.user;
-                setUser(user)
+                setUsers(user)
                 console.log(user)
-            })
+            });
     };
+    const handleSignOut = () => {
+        signOut(auth)
+            .then(() => { })
+    };
+    useEffect(() => {
+        onAuthStateChanged(auth, user => {
+            setUsers(user)
+        })
+    }, [])
     return {
-        user,
+        users,
+        handleSignOut,
         signInWithGoogle
     }
 };
