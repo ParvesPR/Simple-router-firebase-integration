c48516bbea1e61e9e0abf9d302ea0e6b8d377a98
ParvesPR <parves2585@gmail.com>
2022-04-09 12:41:31 +0800

    sign in with google

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import useFirebase from '../../hooks/useFirebase';
 
 const Header = () => {
+    const { user } = useFirebase()
     return (
         <nav>
             <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
             <Link to="/register">Register</Link>
             <Link to="/products">Products</Link>
             <Link to="/contact">Contact</Link>
+            {user.uid ?
+                <button>Sign Out</button> :
+                <Link to="/login">Login</Link>
+            }
         </nav>
     );
 };
--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,15 +1,17 @@
 import React from 'react';
+import useFirebase from '../../hooks/useFirebase';
 
 const Login = () => {
+    const { signInWithGoogle } = useFirebase()
     return (
         <div>
-            <div style={{margin:"20px"}}>
-                <button>Google Sign in</button>
+            <div style={{ margin: "20px" }}>
+                <button onClick={signInWithGoogle}>Google Sign in</button>
             </div>
             <h2>Login Here</h2>
             <form>
-                <input type="email" name="" id="" placeholder='Your email' /> <br />
-                <input type="password" name="" id="" placeholder='password' /> <br />
+                <input type="email" placeholder='Your email' /> <br />
+                <input type="password" placeholder='password' /> <br />
                 <input type="submit" value="Login" />
             </form>
         </div>
